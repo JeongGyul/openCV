8f66073b3b79fe70bc6a9cf6a77fcde8c03ce93f
JeongGyul <jjascd@naver.com>
2023-01-20 11:47:51 +0900

    Edit template.py

--- 실습자료/chapter 13/template.py
+++ 실습자료/chapter 13/template.py
@@ -15,6 +15,11 @@ img = cv2.add(img, noise, dtype=cv2.CV_8UC3)
 # Nomred <-- Normalized
 result = cv2.matchTemplate(img, tmp, cv2.TM_CCOEFF_NORMED)
 
+b_fig = cv2.normalize(result, None,
+                      0, 255,  # 최솟값, 최댓값
+                      cv2.NORM_MINMAX,  # 정규화 기법 선택
+                      cv2.CV_8U)    # 그림의 data type
+
 # Location: 위치
 minv, maxv, minLoc, maxLoc = cv2.minMaxLoc(result)
 
@@ -26,12 +31,12 @@ cv2.rectangle(img,
               maxLoc,   # 점 A
               (maxLoc[0] + tw, maxLoc[1] + th),  # 점 B
               (0, 0, 255), 2)
-print(maxLoc)
+
 # (0, 0, 255) (B, G, R) <-- 빨간색
 # 2 <-- 직사각형을 그릴 때, 선의 두께
 
 cv2.imshow('img', img)
 cv2.imshow('template', tmp)
-cv2.imshow('result', result)
+cv2.imshow('normalized', b_fig)
 cv2.waitKey()
 cv2.destroyAllWindows()
