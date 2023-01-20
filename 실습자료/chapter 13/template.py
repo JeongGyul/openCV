66f20b128577fd8b31d3c884a0707268caae1432
JeongGyul <jjascd@naver.com>
2023-01-20 11:19:11 +0900

    Add template.py

---
+++ 실습자료/chapter 13/template.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('circuit.bmp', cv2.IMREAD_COLOR)
+tmp = cv2.imread('crystal.bmp', cv2.IMREAD_COLOR)
+
+img = img + (50, 50, 50)   # (Blue, Green, Red)
+
+noise = np.zeros(img.shape, np.int32)
+cv2.randn(noise, 0, 10)   # random number --> randn()
+
+img = cv2.add(img, noise, dtype=cv2.CV_8UC3)
+
+# SQ DIFF = Squared Difference
+# Nomred <-- Normalized
+result = cv2.matchTemplate(img, tmp, cv2.TM_CCOEFF_NORMED)
+
+# Location: 위치
+minv, maxv, minLoc, maxLoc = cv2.minMaxLoc(result)
+
+# th <-- template height
+# tw <-- template width
+(th, tw) = tmp.shape[:2]
+
+cv2.rectangle(img,
+              maxLoc,   # 점 A
+              (maxLoc[0] + tw, maxLoc[1] + th),  # 점 B
+              (0, 0, 255), 2)
+print(maxLoc)
+# (0, 0, 255) (B, G, R) <-- 빨간색
+# 2 <-- 직사각형을 그릴 때, 선의 두께
+
+cv2.imshow('img', img)
+cv2.imshow('template', tmp)
+cv2.imshow('result', result)
+cv2.waitKey()
+cv2.destroyAllWindows()
